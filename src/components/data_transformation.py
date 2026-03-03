d3364317044f0ad914b1b5888c1d1eb7fe436d8c
earmahsakyi <sakyi5375@gmail.com>
2026-03-03 14:49:15 +0000

    prediction pipeline

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -10,8 +10,8 @@ from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import OneHotEncoder,StandardScaler
 from sklearn.pipeline import Pipeline
 from src.utils import save_object
-from src.components.data_ingestion import DataIngestion
-from src.components.model_trainer import ModelTrainer
+# from src.components.data_ingestion import DataIngestion
+# from src.components.model_trainer import ModelTrainer
 
 
 
@@ -124,16 +124,16 @@ class DataTransformation:
             raise CustomException(e,sys)
     
 
-if __name__ == '__main__':
-    obj = DataIngestion()
-    train_data_path,test_data_path = obj.initiate_data_ingestion()
+# if __name__ == '__main__':
+#     obj = DataIngestion()
+#     train_data_path,test_data_path = obj.initiate_data_ingestion()
 
-    data_transformation = DataTransformation()
-    train_arr,test_arr,obj_path  = data_transformation.initiate_data_transformation(train_path=train_data_path,test_path=test_data_path)
+#     data_transformation = DataTransformation()
+#     train_arr,test_arr,obj_path  = data_transformation.initiate_data_transformation(train_path=train_data_path,test_path=test_data_path)
 
-    model_trainer = ModelTrainer()
-    score = model_trainer.initiate_model_trainer(train_array=train_arr,test_array=test_arr)
-    print(score)
+#     model_trainer = ModelTrainer()
+#     score = model_trainer.initiate_model_trainer(train_array=train_arr,test_array=test_arr)
+#     print(score)
 
 
 
